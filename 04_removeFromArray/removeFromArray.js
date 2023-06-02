4a10faa07fd19e34a7a9f9e1e1da2bc2f675f8a9
skyeoh <seng.k.yeoh@gmail.com>
2023-06-02 15:51:16 -0400

    Complete exercise 04 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (const item of itemsToRemove) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+                break;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
