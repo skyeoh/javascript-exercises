064918e2aef38c44c77313d644482881ef22ee7e
skyeoh <seng.k.yeoh@gmail.com>
2023-06-02 13:12:05 -0400

    Complete exercise 02 - repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, num) {
+    let outputString = (num < 0) ? 'ERROR' : '';
+    for (let i = 0; i < num; i++) {
+        outputString += inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
