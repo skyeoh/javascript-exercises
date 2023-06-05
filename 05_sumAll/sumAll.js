414eabdb0dd00fc66cb85047bfdc6d77fdbecf5c
skyeoh <seng.k.yeoh@gmail.com>
2023-06-05 14:44:47 -0400

    Complete exercise 05 - sumAll
    A test for non-integer inputs is also added to sumAll.spec.js (taken
    from sumAll-solution.spec.js).

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
 
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    const start = num1 < num2 ? num1 : num2;
+    const end = num1 < num2 ? num2 : num1;
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
