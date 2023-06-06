4b95416eb29ab477bdb68b3eb3c625928e51a5cd
skyeoh <seng.k.yeoh@gmail.com>
2023-06-06 13:03:06 -0400

    Complete exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    let tempC = (tempF - 32) * 5 / 9;
+    tempC = +tempC.toFixed(1);
+    return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+    let tempF = 32 + 9 / 5 * tempC;
+    tempF = +tempF.toFixed(1);
+    return tempF;
 };
 
 // Do not edit below this line
