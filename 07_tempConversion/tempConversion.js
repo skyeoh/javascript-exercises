caa4cc1293498124eeea49c51771c839c5c7b293
skyeoh <seng.k.yeoh@gmail.com>
2023-06-06 13:28:47 -0400

    Change solution to exercise 07
    To round the solution to 1 decimal digit, Math.round(number * 10) / 10
    is used instead of +number.toFixed(1).

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,11 @@
 const convertToCelsius = function(tempF) {
-    let tempC = (tempF - 32) * 5 / 9;
-    tempC = +tempC.toFixed(1);
-    return tempC;
+    const tempC = (tempF - 32) * 5 / 9;
+    return Math.round(tempC * 10) / 10;
 };
 
 const convertToFahrenheit = function(tempC) {
-    let tempF = 32 + 9 / 5 * tempC;
-    tempF = +tempF.toFixed(1);
-    return tempF;
+    const tempF = 32 + 9 / 5 * tempC;
+    return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
